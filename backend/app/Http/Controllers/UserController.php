f4736fb8d65567f54857cc8ff169d4308e390724
Lafy01 <valisoalafy@gmail.com>
2025-01-16 17:20:01 +0300

    list all users success

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function alluser(Request $request)
+    {
+        $users = User::all();
+        return response()->json([
+            'data' => $users,
+            'message' => 'List of all users',
+        ], 200);
+    }
 }
